27706fe703292a56ff8774a1aa39106f446b4d22
mohini deore <mohinideore5@gmail.com>
2023-09-11 15:16:30 +0530

    trek ersion 2 create your tek and login

---
+++ src/trek_planner/Trek_planner.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package trek_planner;
+
+/**
+ *
+ * @author main
+ */
+public class Trek_planner {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
